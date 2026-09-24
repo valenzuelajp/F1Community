001000dc4fb5eed9f6e34662c40364a49b1620ef
Wyntaro-Glitch <sherwincalantoc.developer@gmail.com>
2026-09-24 16:22:36 +0800

    Skill frontend

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,14 +1,5 @@
 'use client';
 
-import { SiteHeader } from '@/components/layout/SiteHeader';
-import { SiteFooter } from '@/components/layout/SiteFooter';
-
 export function AppShell({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <SiteHeader />
-      {children}
-      <SiteFooter />
-    </>
-  );
+  return <>{children}</>;
 }
